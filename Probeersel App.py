1be2bb022f3df6973d0ce34a1e40b0a9e7c345dc
SilentPumpkin <60291753+Homura2003@users.noreply.github.com>
2025-05-22 16:34:22 +0200

    Update Probeersel App.py

--- Probeersel App.py
+++ Probeersel App.py
@@ -11,6 +11,15 @@ from langchain.chat_models import ChatOpenAI
 import os
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_KEY"]
 
+llm = LangChainInterface(
+    credentials=creds,
+    model='ollama run mistral:instruct',
+    params = {
+        'decoding_method':'sample',
+        'max_new_tokens':200,
+        'temperature':0.5
+    })   
+
 st.title('Radiologie chatbot')
 
 if 'messages' not in st.session_state:
@@ -22,3 +31,8 @@ prompt = st.chat_input('Pass your prompt here')
 if prompt:
     st.chat_message('user').markdown(prompt)
     st.session_state.messages.append({'role':'user', 'content':prompt})
+    response = llm(prompt)
+    st.chat_message('assistant').markdown(response)
+    st.session_state.message.append(
+        {'role':'assistant', 'content':response})
+    
