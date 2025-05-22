12efc7bbbf2b1e8c2ed2b795e5d7ee6aaf874eb6
SilentPumpkin <60291753+Homura2003@users.noreply.github.com>
2025-05-22 16:59:23 +0200

    Update Probeersel App.py

--- Probeersel App.py
+++ Probeersel App.py
@@ -3,6 +3,7 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import retrieval_qa
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import HumanMessage, AIMessage
 
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
@@ -23,8 +24,10 @@ prompt = st.chat_input('Pass your prompt here')
 if prompt:
     st.chat_message('user').markdown(prompt)
     st.session_state.messages.append({'role':'user', 'content':prompt})
-    response = llm(prompt)
-    st.chat_message('assistant').markdown(response)
-    st.session_state.message.append(
-        {'role':'assistant', 'content':response})
     
+    messages = [HumanMessage(content=prompt)]
+    response = llm(messages)
+    
+    st.chat_message('assistant').markdown(response.content)
+    st.session_state.messages.append(
+        {'role':'assistant', 'content':response.content})
