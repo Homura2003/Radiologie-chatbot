22ca113f2a73cff940fd60cb5233a633e9834836
SilentPumpkin <60291753+Homura2003@users.noreply.github.com>
2025-05-22 16:16:52 +0200

    Update Probeersel App.py

--- Probeersel App.py
+++ Probeersel App.py
@@ -1,14 +1,11 @@
-# Importing langchain dependencies
 from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import retrieval_qa
-from langchain.embeddings import OpenAIEmbeddings  # <-- vervangen
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-# Streamlit voor UI
 import streamlit as st
 
-# OpenAI LLM toevoegen
 from langchain.chat_models import ChatOpenAI
 
 import os
@@ -16,7 +13,11 @@ os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_KEY"]
 
 st.title('Radiologie chatbot')
 
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
 prompt = st.chat_input('Pass your prompt here')
 
 if prompt:
     st.chat_message('user').markdown(prompt)
+    st.session_state.messages.append({'role':'user', 'content':prompt})
