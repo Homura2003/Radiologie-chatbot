5301c8aaea9d79b92d9bad0fe6f18aecb7a9d26a
SilentPumpkin <60291753+Homura2003@users.noreply.github.com>
2025-05-23 10:37:49 +0200

    Different model

--- Probeersel App.py
+++ Probeersel App.py
@@ -12,7 +12,7 @@ import os
 os.environ["HUGGINGFACE_API_KEY"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
 llm = HuggingFaceEndpoint(
-    endpoint_url="https://api-inference.huggingface.co/models/BramVanroy/GEITje-7B-ultra",
+    endpoint_url="https://api-inference.huggingface.co/models/BramVanroy/fietje-2",
     task="text-generation",
     temperature=0.8,
     top_p=0.9,
@@ -44,6 +44,7 @@ if prompt:
         st.session_state.messages.append(
             {'role':'assistant', 'content':error_message})
     
+
 
     
 
