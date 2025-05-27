ab08a88f0119d6ee4a4f27c23c00b976399ade70
SilentPumpkin <60291753+Homura2003@users.noreply.github.com>
2025-05-27 11:12:39 +0200

    Terug bij perplexity

--- Probeersel App.py
+++ Probeersel App.py
@@ -1,10 +1,7 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.chains import retrieval_qa
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import HumanMessage, AIMessage
-from transformers import AutoModelForCausalLM
+
 
 import streamlit as st
 from langchain_huggingface import HuggingFaceEndpoint
@@ -12,7 +9,6 @@ from langchain_huggingface import HuggingFaceEndpoint
 import os
 os.environ["HUGGINGFACE_API_KEY"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
-model = AutoModelForCausalLM.from_pretrained("BramVanroy/GEITje-7B-ultra") 
 llm = HuggingFaceEndpoint(
     repo_id="BramVanroy/GEITje-7B-ultra",
     task="text-generation",
@@ -21,6 +17,7 @@ llm = HuggingFaceEndpoint(
     do_sample=True,
     max_new_tokens=512
 )
+
 st.title('Radiologie chatbot')
 
 if 'messages' not in st.session_state:
@@ -32,10 +29,10 @@ prompt = st.chat_input('Stel hier je vraag')
 if prompt:
     st.chat_message('user').markdown(prompt)
     st.session_state.messages.append({'role':'user', 'content':prompt})
-    
+
     try:
         response = llm.invoke(prompt)
-        
+
         st.chat_message('assistant').markdown(response)
         st.session_state.messages.append(
             {'role':'assistant', 'content':response})
@@ -44,7 +41,6 @@ if prompt:
         st.chat_message('assistant').markdown(error_message)
         st.session_state.messages.append(
             {'role':'assistant', 'content':error_message})
-    
 
     
 
