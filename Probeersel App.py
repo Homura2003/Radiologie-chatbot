349ad10cd134593c097349c92755369ddbf04c47
SilentPumpkin <60291753+Homura2003@users.noreply.github.com>
2025-05-27 12:54:30 +0200

    Terug naar PyTorch

--- Probeersel App.py
+++ Probeersel App.py
@@ -4,7 +4,7 @@ from langchain.chains import retrieval_qa
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import HumanMessage, AIMessage
-from transformers import pipeline, TFAutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
 import streamlit as st
 from langchain_huggingface import HuggingFaceEndpoint
@@ -15,13 +15,12 @@ os.environ["HUGGINGFACE_API_KEY"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 try:
     model_name = "GroNLP/gpt2-small-dutch"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = TFAutoModelForCausalLM.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
     
     pipe = pipeline(
         "text-generation",
         model=model,
-        tokenizer=tokenizer,
-        framework="tf"
+        tokenizer=tokenizer
     )
 
     st.title('Radiologie chatbot')
